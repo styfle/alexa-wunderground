eb39b6a6dac86ce5cec8e880ff886c78172fb28b
Steven <steven@ceriously.com>
2017-12-27 21:45:12 -0500

    Add node-fetch an experimental alexa zip code

--- index.js
+++ index.js
@@ -1,36 +1,105 @@
 // @ts-check
 const http = require('http');
+const fetch = require('node-fetch');
 
-const hostname = '0.0.0.0';
-const port = process.env.PORT || 3012;
+const HOSTNAME = '0.0.0.0';
+const PORT = process.env.PORT || 3012;
+const API_KEY = process.env.API_KEY;
 
-const server = http.createServer((req, res) => {
-    let raw = '';
-    req.on('data', chunk => { raw += chunk; });
-    req.on('end', () => {
-        console.log('ip: ', req.connection.remoteAddress);
-        console.log('method: ', req.method);
-        console.log('url: ', req.url);
-        
-        if (raw) {
-            console.log('raw data: ', raw);
-            const data = JSON.parse(raw);
-            // TODO: do stuff with data
-        }
-        console.log('\n');
-        
-        const weather = getWeather();
-        const json = getResponseJSON(weather);
-        res.statusCode = 200;
+const server = http.createServer(async (req, res) => {
+    try {
+        let raw = '';
+        req.on('data', chunk => { raw += chunk; });
+        req.on('end', () => {
+            console.log('ip: ', req.connection.remoteAddress);
+            console.log('method: ', req.method);
+            console.log('url: ', req.url);
+            
+            if (raw) {
+                console.log('raw data: ', raw);
+                const data = JSON.parse(raw);
+                const alexaResponse = getZip(data);
+                console.log(alexaResponse);
+                const json = JSON.stringify(alexaResponse);
+                /*
+                let city = 'San Francisco';
+
+                if (isIntent(data, 'WundergroundIntent')) {
+                    city = getSlotValue(data, 'city');
+                } else if (isIntent(data, 'AMAZON.SearchAction<object@WeatherForecast>')) {
+                    city = getSlotValue(data, 'object.location.addressLocality.name');
+                }
+
+                const weather = getWeather(city);
+                const json = getResponseJSON(weather);
+                */
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json;charset=UTF-8');
+                res.setHeader('Content-Length', json.length);
+                res.end(json);
+            } else {
+                const obj = { status: 'ok' };
+                const json = JSON.stringify(obj);
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json;charset=UTF-8');
+                res.setHeader('Content-Length', json.length);
+                res.end(json);
+            }
+            console.log('\n');
+        });
+    } catch (e) {
+        const obj = { error: e.message };
+        const json = JSON.stringify(obj);
+        res.statusCode = 500;
         res.setHeader('Content-Type', 'application/json;charset=UTF-8');
         res.setHeader('Content-Length', json.length);
         res.end(json);
-    });
-}).listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
+    }
+}).listen(PORT, HOSTNAME, () => {
+    console.log(`Server running at http://${HOSTNAME}:${PORT}`);
 });
 
-function getWeather() {
+function isIntent(data, name) {
+    try {
+        return (data.request.intent.name === name);
+    } catch (e) {
+        return false;
+    }
+}
+
+function getSlotValue(data, slot) {
+    return data.request.intent.slots[slot].value;
+}
+
+function getState(city) {
+    const url = `https://api.wunderground.com/api/${API_KEY}/conditions/q/US/${city}json`;
+}
+
+function getForecast(zip) {
+    const { lat, lng } = getLatLng(zip);
+    const url = `https://api.wunderground.com/api/${API_KEY}/conditions/forecast/q/${lat},${lng}.json`;
+}
+
+function getLatLng(zip) {
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${zip}`;
+
+    const data = {}; // get from response
+    return data.results[0].geometry.location;
+}
+
+async function getZip(data) {
+    const r = await fetch(`${data.context.System.apiEndpoint}/v1/devices/${data.context.System.device.deviceId}/settings/address/countryAndPostalCode`, { 
+        method: 'GET', 
+        headers: {
+            'Authorization': 'Basic ' + data.context.System.apiAccessToken, 
+            'Content-Type': 'application/json'
+        }
+    });
+    const obj = await r.json();
+    return obj;
+}
+
+function getWeather(city) {
     const f = {
         "date": {
             "epoch": "1514160000",
@@ -106,8 +175,6 @@ function getWeather() {
         "minhumidity": 0
     };
     f.icon_url = `https://icons.wxug.com/i/c/v4/${f.icon}.svg`;
-    f.high.fahrenheit
-    f.snow_allday.in
     return f;
 }
 
@@ -141,3 +208,23 @@ function getResponseJSON(f) {
     };
     return JSON.stringify(o);
 }
+/*
+const https = require('https');
+function getAsync(url) {
+    return new Promise((resolve, reject) => {
+        https.get(url, (res) => {
+            console.log('statusCode:', res.statusCode);
+            console.log('headers:', res.headers);
+            let data = '';
+            res.on('data', (d) => { data += d; });
+            res.on('end', () => {
+                try {
+                    resolve(JSON.parse(data));
+                } catch(e) {
+                    reject(e);
+                }
+            });
+        }).on('error', e => reject(e));
+    });
+}
+*/
